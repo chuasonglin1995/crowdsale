6a0e4bf9beb8f8df1350d3932a8a9c354d12cd66
chuasonglin1995 <chuasonglin1995@gmail.com>
2023-11-05 23:41:55 +0800

    complete simpleCrowdsale contract

--- contracts/SimpleCrowdsale.sol
+++ contracts/SimpleCrowdsale.sol
@@ -14,6 +14,78 @@ contract SimpleCrowdsale {
     bool public isFinalized;
     bool public isRefundingAllowed;
 
-    
+    ReleasableSimpleCoin public crowdsaleToken;
 
+    constructor(uint256 _startTime, uint256 _endTime, uint256 _weiTokenPrice, uint256 _weiInvestmentObjective) payable public {
+        require(_startTime >= now);
+        require(_endTime >= _startTime);
+        require(_weiTokenPrice != 0);
+        require(_weiInvestmentObjective != 0);
+
+        startTime = _startTime;
+        endTime = _endTime;
+        weiTokenPrice = _weiTokenPrice;
+        weiInvestmentObjective = _weiInvestmentObjective;
+
+        crowdsaleToken = new ReleaseableSimpleCoin(0);
+        isFinalized = false;
+    }
+
+    event LogInvestment(address indexed investor, uint256 value);
+    event LogTokenAssignment(address indexed investor, uint256 numTokens);
+    event Refund(address investor, uint256 value);
+
+    function invest() public payable {
+        require(isValidInvestment(msg.value));
+
+        address investor = msg.sender;
+        uint256 investment = msg.value;
+
+        investmentAmountOf[investor] += investment;
+        investmentReceived += investment;
+
+        assignTokens(investor, investment);
+        emit LogInvestment(investor, investment);
+    }
+
+    function isValidInvestment(uint256 _investment) internal view returns (bool) {
+        bool nonZeroInvestment = _investment != 0;
+        bool withinCrowdsalePeriod = now >= startTime && now <= endTime;
+
+        return nonZeroInvestment && withinCrowdsalePeriod;
+    }
+
+    function calculateNumberOfTokens(uint256 _investment) internal returns (uint256) {
+        return _investment / weiTokenPrice;
+    }
+
+    function finalize() onlyOwner public {
+        if (isFinalized) revert();
+        
+        bool isCrowdsaleComplete = now > endTime;
+        bool isInvestmentObjectiveMet = investmentReceived >= weiInvestmentObjective;
+
+        if (isCrowdsaleComplete) {
+            if (isInvestmentObjectiveMet) {
+                crowdsaleToken.release();
+            } else {
+                isRefundingAllowed = true;
+            }
+
+            isFinalized = true;
+        }
+    }
+
+    function refund() public {
+        if (!isRefundingAllowed) revert();
+
+        address investor = msg.sender;
+        uint256 investment = investmentAmountOf[investor];
+        if (investment == 0) revert();
+        investmentAmountOf[investor] = 0;
+        investmentRefunded += investment;
+        emit Refund(msg.sender, investment);
+
+        if (!investor.send(investment)) revert();
+    }
 }
