24263b7fc7c6c72872603bcc3a2485c2b094a4c1
chuasonglin1995 <chuasonglin1995@gmail.com>
2023-11-06 23:09:05 +0800

    fix simpleCrowdsale

--- contracts/SimpleCrowdsale.sol
+++ contracts/SimpleCrowdsale.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 import "./ReleasableSimpleCoin.sol";
+import "./Ownable.sol";
 
-contract SimpleCrowdsale {
+contract SimpleCrowdsale is Ownable {
     uint256 public startTime;
     uint256 public endTime;
     uint256 public weiTokenPrice;
@@ -17,8 +18,8 @@ contract SimpleCrowdsale {
 
     ReleasableSimpleCoin public crowdsaleToken;
 
-    constructor(uint256 _startTime, uint256 _endTime, uint256 _weiTokenPrice, uint256 _weiInvestmentObjective) payable public {
-        require(_startTime >= now);
+    constructor(uint256 _startTime, uint256 _endTime, uint256 _weiTokenPrice, uint256 _weiInvestmentObjective) payable {
+        require(_startTime >= block.timestamp);
         require(_endTime >= _startTime);
         require(_weiTokenPrice != 0);
         require(_weiInvestmentObjective != 0);
@@ -28,7 +29,7 @@ contract SimpleCrowdsale {
         weiTokenPrice = _weiTokenPrice;
         weiInvestmentObjective = _weiInvestmentObjective;
 
-        crowdsaleToken = new ReleaseableSimpleCoin(0);
+        crowdsaleToken = new ReleasableSimpleCoin(0);
         isFinalized = false;
     }
 
@@ -51,9 +52,14 @@ contract SimpleCrowdsale {
 
     function isValidInvestment(uint256 _investment) internal view returns (bool) {
         bool nonZeroInvestment = _investment != 0;
-        bool withinCrowdsalePeriod = now >= startTime && now <= endTime;
+        bool withinCrowdsalePeriod = block.timestamp >= startTime && block.timestamp <= endTime;
 
         return nonZeroInvestment && withinCrowdsalePeriod;
+    }
+
+    function assignTokens(address _beneficiary, uint256 _investment) internal {
+        uint256 _numberOfTokens = calculateNumberOfTokens(_investment);
+        crowdsaleToken.mint(_beneficiary, _numberOfTokens);
     }
 
     function calculateNumberOfTokens(uint256 _investment) internal returns (uint256) {
@@ -63,7 +69,7 @@ contract SimpleCrowdsale {
     function finalize() onlyOwner public {
         if (isFinalized) revert();
         
-        bool isCrowdsaleComplete = now > endTime;
+        bool isCrowdsaleComplete = block.timestamp > endTime;
         bool isInvestmentObjectiveMet = investmentReceived >= weiInvestmentObjective;
 
         if (isCrowdsaleComplete) {
@@ -80,13 +86,13 @@ contract SimpleCrowdsale {
     function refund() public {
         if (!isRefundingAllowed) revert();
 
-        address investor = msg.sender;
+        address payable investor = payable(msg.sender);
         uint256 investment = investmentAmountOf[investor];
         if (investment == 0) revert();
         investmentAmountOf[investor] = 0;
         investmentRefunded += investment;
         emit Refund(msg.sender, investment);
 
-        if (!investor.send(investment)) revert();
+        investor.transfer(investment);
     }
 }
